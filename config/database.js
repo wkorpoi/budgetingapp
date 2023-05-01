b4200f71bda96704a3957f4c7e5fb62805d32f95
wadiya korpoi <wadiyakorpoi@gmail.com>
2023-04-30 20:26:45 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://wadiyakorpoi:budget123@cluster0.fqgxg3f.mongodb.net/demo?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
