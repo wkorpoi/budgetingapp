085e30aee68ca0f6dce18f99323e43f4b2e933b4
wadiya korpoi <wadiyakorpoi@gmail.com>
2023-05-05 17:29:14 -0400

    updated project

--- app/routes.js
+++ app/routes.js
@@ -1,121 +1,127 @@
-module.exports = function(app, passport, db) {
+const ObjectId = require("mongodb").ObjectId;
+module.exports = function (app, passport, db) {
+  // normal routes ===============================================================
 
-// normal routes ===============================================================
+  // show the home page (will also have our login links)
+  app.get("/", function (req, res) {
+    res.render("index.ejs");
+  });
 
-    // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
-    });
-
-    // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, function(req, res) {
-        db.collection('messages').find().toArray((err, result) => {
-          if (err) return console.log(err)
-          res.render('profile.ejs', {
-            user : req.user,
-            messages: result
-          })
-        })
-    });
-
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-        req.logout(() => {
-          console.log('User has logged out!')
+  // PROFILE SECTION =========================
+  app.get("/profile", isLoggedIn, function (req, res) {
+    db.collection("messages")
+      .find()
+      .toArray((err, result) => {
+        if (err) return console.log(err);
+        res.render("profile.ejs", {
+          user: req.user,
+          messages: result,
         });
-        res.redirect('/');
+      });
+  });
+
+  // LOGOUT ==============================
+  app.get("/logout", function (req, res) {
+    req.logout(() => {
+      console.log("User has logged out!");
     });
+    res.redirect("/");
+  });
 
-// message board routes ===============================================================
+  // message board routes ===============================================================
 
-    app.post('/messages', (req, res) => {
-      db.collection('messages').save({name: req.body.name, date: req.body.date, amount: req.body.amount}, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/profile')
-      })
+  app.post("/messages", (req, res) => {
+    const key = "nXCmmp1RY-xTLkf7kX-QONb62EwBwXQjmIYn5EIY2IU";
+    const imageUrl = `https://api.unsplash.com/search/photos/?query=${req.body.name}&client_id=${key}&page=1&per_page=1`;
+    fetch(imageUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results[0].urls.regular);
+
+    db.collection("messages").save(
+      {
+        name: req.body.name,
+        date: req.body.date,
+        amount: req.body.amount,
+        image: data.results[0].urls.regular
+      },
+      (err, result) => {
+        if (err) return console.log(err);
+        console.log("saved to database");
+        res.redirect("/profile");
+      }
+    );
+});
+  });
+
+  app.delete("/delete", (req, res) => {
+    db.collection("messages").findOneAndDelete(
+      { _id: ObjectId(req.body._id) },
+      (err, result) => {
+        if (err) return res.send(500, err);
+        res.send("Message deleted!");
+      }
+    );
+  });
+
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get("/login", function (req, res) {
+    res.render("login.ejs", { message: req.flash("loginMessage") });
+  });
+
+  // process the login form
+  app.post(
+    "/login",
+    passport.authenticate("local-login", {
+      successRedirect: "/profile", // redirect to the secure profile section
+      failureRedirect: "/login", // redirect back to the signup page if there is an error
+      failureFlash: true, // allow flash messages
     })
+  );
 
-    // app.put('/messages', (req, res) => {
-    //   db.collection('messages')
-    //   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    //     $set: {
-    //       thumbUp:req.body.thumbUp + 1
-    //     }
-    //   }, {
-    //     sort: {_id: -1},
-    //     upsert: true
-    //   }, (err, result) => {
-    //     if (err) return res.send(err)
-    //     res.send(result)
-    //   })
-    // })
+  // SIGNUP =================================
+  // show the signup form
+  app.get("/signup", function (req, res) {
+    res.render("signup.ejs", { message: req.flash("signupMessage") });
+  });
 
-    app.delete('/messages', (req, res) => {
-      console.log('working')
-      console.log(req.body.name)
-      console.log(req.body.date)
-      console.log(req.body.amount)
-      db.collection('messages').findOneAndDelete({name: req.body.name, date: req.body.date, amount: req.body.amount}, (err, result) => {
-        if (err) return res.send(500, err)
-        res.send('Message deleted!')
-      })
+  // process the signup form
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/profile", // redirect to the secure profile section
+      failureRedirect: "/signup", // redirect back to the signup page if there is an error
+      failureFlash: true, // allow flash messages
     })
+  );
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
 
-    // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
-
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
-
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
-
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
-
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
-
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
+  // local -----------------------------------
+  app.get("/unlink/local", isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect("/profile");
     });
-
+  });
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
+  if (req.isAuthenticated()) return next();
 
-    res.redirect('/');
+  res.redirect("/");
 }
--- public/main.js
+++ public/main.js
@@ -1,50 +1,18 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("delete");
-
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
-});
+const trash = document.getElementsByClassName("delete");
 
 Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = document.querySelector("#hello").innerText;
-        const date = document.querySelector("#goodbye").innerText;
-        const amount = document.querySelector("#money").innerText;
+      element.addEventListener('click', function(event){
+      const _id = event.target.value
         console.log('working')
-        console.log(name)
-  
-        fetch('messages', {
+        window.location.reload()
+        fetch('delete', {
           method: 'delete',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            'name': name,
-            'date': date,
-            'amount':amount,
+            _id
           })
-        }).then(function (response) {
-          window.location.reload()
         })
       });
 });
